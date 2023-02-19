a7e8c2e61bfadf17d625e7ccfcd91975992c9964
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 12:39:51 +0100

    Restaurant model updated

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,5 +1,15 @@
 
 
+import 'package:flutter/cupertino.dart';
+
 class Restaurant{
-  
+  String name;
+  String waitTime;
+  String distance;
+  String label;
+  String logoUrl;
+  String desc;
+  num score;
+  Map<String, List<Food>> menu;
+  Restaurant();
 }
