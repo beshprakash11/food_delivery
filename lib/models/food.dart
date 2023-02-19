f533b63a742942988f00326e27f0423283efa772
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 12:44:28 +0100

    Food updated

---
+++ lib/models/food.dart
@@ -0,0 +1,15 @@
+
+class Food{
+  String imgUrl;
+  String desc;
+  String name;
+  String waitTime;
+  String score;
+  String cal;
+  num price;
+  num quantity;
+  List<Map<String, String>> ingredients;
+  String about;
+  String hightLight;
+  
+}
