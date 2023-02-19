12061d25203dba8a2d7f8295b803b1c170516360
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 12:08:09 +0100

    Main pages updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Food Delivery App',
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
