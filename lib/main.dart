f8af347adfc75da717649eef8f68fef12c3256cd
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 12:10:20 +0100

    Main pages updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:food_delivery/screens/home/home.dart';
 
 void main() {
@@ -10,6 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent
+      )
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Food Delivery App',      
