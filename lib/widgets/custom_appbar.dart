89e119b6b5697920ac05f16055d2b514120a4c27
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 12:35:01 +0100

    Code refactor

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -19,14 +19,7 @@ class CustomAppBar extends StatelessWidget {
         children: [
           GestureDetector(
             onTap: leftCallback != null ? () => leftCallback!() : null,
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.white
-              ),
-              child: Icon(leftIcon),
-            ),
+            child: _buildIcon(leftIcon),
           ),
           Container(
             padding: EdgeInsets.all(8),
@@ -38,6 +31,17 @@ class CustomAppBar extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+
+  Widget _buildIcon(IconData icon) {
+    return Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white
+        ),
+        child: Icon(icon),
     );
   }
 }
