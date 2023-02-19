2d8a2449ccbeca2b6a19436348d8d40d4a601213
beshprakash11 <beshprakashktm@gmail.com>
2023-02-19 12:24:39 +0100

    lefticon

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -14,6 +14,19 @@ class CustomAppBar extends StatelessWidget {
         top: MediaQuery.of(context).padding.top,
         left: 25,
       ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 8, 8, 8),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white
+            ),
+            child: Icon(leftIcon),
+          ),
+        ],
+      ),
     );
   }
 }
