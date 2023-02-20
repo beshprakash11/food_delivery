20653f4b3e708153f53396d46670649f2141d17c
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 14:40:05 +0100

    Align widget added

--- lib/screens/details/widget/food_quantity.dart
+++ lib/screens/details/widget/food_quantity.dart
@@ -12,7 +12,17 @@ class FoodQuantity extends StatelessWidget {
       height: 40,
       child: Stack(
         children: [
-          Text("food qty"),
+          Align(
+            alignment: Alignment(-0.3, 0),
+            child: Container(
+              width: 120,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+          )
         ],
       ),
     );
