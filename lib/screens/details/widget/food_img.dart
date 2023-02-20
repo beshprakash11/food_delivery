6fe39977cf0607c5406bba270e2fca632746fcb3
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 13:03:11 +0100

    FoodImg pages updatd

---
+++ lib/screens/details/widget/food_img.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FoodImg extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Fod imae'),
+    );
+  }
+}
