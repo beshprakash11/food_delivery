380edc6ca182a24cae7878ef138c209ce2844df3
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 12:55:55 +0100

    Detail pages updated

--- lib/screens/details/detail.dart
+++ lib/screens/details/detail.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../models/food.dart';
+
 class DetilPage extends StatelessWidget {
-  const DetilPage({super.key});
+  final Food food;
+  
+  DetilPage(this.food);
 
   @override
   Widget build(BuildContext context) {
