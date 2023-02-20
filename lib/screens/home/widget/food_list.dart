1e47286bd9344ee116258936da88d362b784685c
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 07:40:13 +0100

    Food list widget updated

--- lib/screens/home/widget/food_list.dart
+++ lib/screens/home/widget/food_list.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/models/restaurant.dart';
 
 class FoodList extends StatelessWidget {
   final int selected;
   final Function callback;
-  FoodList(this.selected, this.callback);
+  final Restaurant restaurant;
+  FoodList(this.selected, this.callback, this.restaurant);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('List'),
+    );
   }
 }
