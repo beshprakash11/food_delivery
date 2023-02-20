ac2a7c8a254ced0c7517844b652844c34cd0533e
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 12:51:06 +0100

    Detail pages added

---
+++ lib/screens/details/detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetilPage extends StatelessWidget {
+  const DetilPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
