590f1b67d5bbea99c63131435725d9c20d7a19ca
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 07:23:20 +0100

    Code refactor

--- lib/screens/home/widget/restaurant_inf.dart
+++ lib/screens/home/widget/restaurant_inf.dart
@@ -12,65 +12,69 @@ class RestaurantInfo extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 25),
       child: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    resturaant.name,
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.withOpacity(0.4),
-                          borderRadius: BorderRadius.circular(5)
-                        ),
-                        child: Text(
-                          resturaant.waitTime,
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
-                        )
-                      ),
-                      SizedBox(width: 10,),
-                      Text(
-                        resturaant.distance,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey.withOpacity(0.4)
-                        ),
-                      ),
-                      SizedBox(width: 10,),
-                      Text(
-                        resturaant.label,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey.withOpacity(0.4)
-                        )
-                      )
-                    ],
-                  )
-                ],
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: Image.asset(resturaant.logoUrl, width: 80,),
-              ),
-            ],
-          ),
+          _firstRow(resturaant),
         ],
       ),
     );
+  }
+
+  Widget _firstRow(Restaurant resturaant) {
+    return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  resturaant.name,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.4),
+                        borderRadius: BorderRadius.circular(5)
+                      ),
+                      child: Text(
+                        resturaant.waitTime,
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      )
+                    ),
+                    SizedBox(width: 10,),
+                    Text(
+                      resturaant.distance,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.withOpacity(0.4)
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Text(
+                      resturaant.label,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.withOpacity(0.4)
+                      )
+                    )
+                  ],
+                )
+              ],
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset(resturaant.logoUrl, width: 80,),
+            ),
+          ],
+        );
   }
 }
