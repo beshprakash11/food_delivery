1be5f3bffe58c72ac1fab4e5787c37b6403c507d
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 13:04:59 +0100

    FoodImg pages updatd

--- lib/screens/details/widget/food_img.dart
+++ lib/screens/details/widget/food_img.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/models/food.dart';
 
 class FoodImg extends StatelessWidget {
-  
+  final Food food;
+  FoodImg(this.food);
 
   @override
   Widget build(BuildContext context) {
