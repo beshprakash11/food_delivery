dc4da6c1db7fd2f92069e62445d408ccda6d15da
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 13:17:12 +0100

    Food detail pages updated

--- lib/screens/details/widget/food_detail.dart
+++ lib/screens/details/widget/food_detail.dart
@@ -5,6 +5,8 @@ class FoodDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('Food details'),
+    );
   }
 }
