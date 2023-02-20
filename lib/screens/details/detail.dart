7484fe9dd285653409aa3d814208853649635079
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 12:53:20 +0100

    Detail pages updated

--- lib/screens/details/detail.dart
+++ lib/screens/details/detail.dart
@@ -5,6 +5,10 @@ class DetilPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text('Detail Pages'),
+      ),
+    );
   }
 }
