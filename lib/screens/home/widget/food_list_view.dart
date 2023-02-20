b53505b021667240606c90fe5e7351aab238ed25
beshprakash11 <beshprakashktm@gmail.com>
2023-02-20 08:07:43 +0100

    Food list view updated

---
+++ lib/screens/home/widget/food_list_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/models/restaurant.dart';
+
+class FoodListView extends StatelessWidget {
+  final int selected;
+  final Function callback;
+  final PageController pageController;
+  final Restaurant restaurant;
+  FoodListView(this.selected, this.callback, this.pageController, this.restaurant);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
